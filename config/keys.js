b4ba9c4462bb9af114ea91fe34e554cd4c492780
Yudhajitadhikary <35257370+Yudhajitadhikary@users.noreply.github.com>
2020-02-24 22:57:35 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+        google: {
+            clientID: '<PUT YOUR CLIENT ID>',
+            clientSecret: '<PUT YOUR CLIENTSECRET>'
+        },
+        mongodb: {
+            dbURI: 'mongodb://localhost:27017/<Name of Project>'
+        },
+        session: {
+            cookieKey: '<put your cookie key>'
+        }
+};
